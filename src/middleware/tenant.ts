9e5f9b0c60c162843880b600ccb3170d20f791c7
bellovinicius <bellovinicius@me.com>
2026-07-15 16:54:29 -0300

    Update tenant.ts

--- src/middleware/tenant.ts
+++ src/middleware/tenant.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { AuthRequest } from '../types';
 
 export const tenantMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
-  if (!req.empresa_id) {
+  if (!req.empresaId) {
     return res.status(401).json({ success: false, message: 'Tenant ID não identificado' });
   }
   next();
