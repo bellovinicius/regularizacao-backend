c0f00167eb313cf0a02b78d46ad0f2cab09322f4
bellovinicius <bellovinicius@me.com>
2026-07-15 12:45:52 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import express from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get('/health', (req, res) => {
-  res.json({ success: true, message: 'API OK', statusCode: 200 });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/health", (req: Request, res: Response) => {
+  res.json({ success: true });
 });
